3fbfc59cfd19ea1e48a331c325fbc7183bc65672
fiaeb23 <fiae.b.23@gmail.com>
2023-06-30 12:05:37 +0200

    PY2

---
+++ vorgaben/07_02_Datenbank_II_III_Python_CSV_00_LEER_2023_Winter_FIAE_B.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from IPython.display import Image
+Image(filename='g001.png')
+
+Aufgabe 1:
+Sie bekommen zur Verfügung die Datei "Iris.csv". Importieren Sie den Iris-Datensatz und zeigen Sie die Daten wie auf dem vorherigen Bild
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+from IPython.display import Image
+Image(filename='g002.png')
+
+Aufgabe 2:
+mit welchem Befehl können Sie die Unterteilung der Datei "iris.csv" wie auf dem vorherigen Bild ausführen ? 
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+from IPython.display import Image
+Image(filename='g003.png')
+
+Aufgabe 3:
+Finden Sie den Code heraus, um den Datensatz wie auf dem vorherigen Bild auszuführen
+# In[ ]:
+
+
+
+
+Aufgabe 4:
+Aus der ersten 150 Zeilen zählen Sie, wie viele Unterteilung von 
+"Iris-setosa" dabei sind ?
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+from IPython.display import Image
+Image(filename='g005.png')
+
+Aufgabe 5:
+Finden Sie den Code heraus, um den Datensatz wie auf dem vorherigen Bild auszuführen
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+###############################################
+
+
+# In[29]:
+
+
+#Importieren des Iris-Datensatzes
+iris = pd.read_csv('Iris.csv', sep=';')
+
+Aufgabe 6:
+Holen Sie die Werte aus der Zeile 0 von "sepallength" und "sepalwidth"  heraus
+# In[ ]:
+
+
+
+
+Aufgabe 7:
+Holen Sie die Werte aus der Zeile 0 und 1 von "sepallength" heraus
+# In[ ]:
+
+
+
+
+Aufgabe 8:
+Holen Sie die Werte aus der Zeile 0 von "sepallength", "sepalwidth", "petallength" , "petalwidth" und "class"  heraus
+# In[ ]:
+
+
+
+
+Aufgabe 9:
+Holen Sie alle Werte kleiner als 4.9 aus der ersten 100 Zeilen von "sepallength"  heraus
+# In[ ]:
+
+
+
+
+Aufgabe 10:
+Holen Sie alle Werte kleiner als 4.9 von "sepallength" und gleich 3 von "sepalwidth"  aus der ersten 100 Zeilen heraus
+# In[ ]:
+
+
+
+
